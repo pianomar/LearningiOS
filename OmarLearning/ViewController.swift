ce37f1b280defb073079faf649b14af22b896296
Omar <Omar@bakers-macbook-pro.local>
2018-10-27 14:24:11 -0300

    Initial Commit

---
+++ OmarLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OmarLearning
+//
+//  Created by Omar on 27/10/2018.
+//  Copyright © 2018 wifiesta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
